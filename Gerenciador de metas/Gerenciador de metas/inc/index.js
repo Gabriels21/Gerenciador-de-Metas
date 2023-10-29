a6c0cf4755d32fbab65d91fa08699f06c6ebc779
Gabriel dos Santos Santana <92961406+Gabriels21@users.noreply.github.com>
2023-10-28 23:07:48 -0300

    Delete Gerenciador de metas directory

--- Gerenciador de metas/Gerenciador de metas/inc/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#switch').addEventListener('click',()=>{
-    document.body.classList.toggle('dark');
-})
-
-function excluir(id){
-    if(confirm("Deseja excluir esta meta?")){
-        window.location.href = "index.php?excluir=" + id;
-    }
-}
